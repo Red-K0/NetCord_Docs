4276b54d30adfb714f00cfca53b9b5c58276f9ed
Kuba_Z2 <77853483+KubaZ2@users.noreply.github.com>
2024-12-29 15:47:15 +0100

    Use ApplicationCommandContext where it's possible (#84)
    * Use ApplicationCommandContext where it's possible
    
    * Shorten the name for IDataProvider variables
    
    * Improve wording
    
    * Use `ISomeService` instead of `IDataProvider`
    
    * Move the multiple services guide to the bottom and improve its wording

--- Documentation/guides/services/DependencyInjection/Program.cs
+++ Documentation/guides/services/DependencyInjection/Program.cs
@@ -15,10 +15,10 @@ using NetCord.Services.Commands;
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services
-    .AddSingleton<IDataProvider, DataProvider>()
+    .AddSingleton<ISomeService, SomeService>()
     .AddDiscordGateway(o => o.Intents = GatewayIntents.GuildMessages | GatewayIntents.DirectMessages | GatewayIntents.MessageContent)
     .AddCommands<CommandContext>()
-    .AddApplicationCommands<SlashCommandInteraction, SlashCommandContext>();
+    .AddApplicationCommands<ApplicationCommandInteraction, ApplicationCommandContext>();
 
 var host = builder.Build();
 
@@ -27,8 +27,9 @@ host.AddModules(typeof(Program).Assembly);
 host.AddSlashCommand(
         name: "data",
         description: "Shows the data!",
-        (IDataProvider dataProvider, SlashCommandContext context, int count) => string.Join(' ', dataProvider.GetData()
-                                                                                                             .Take(count)));
+        (ISomeService someService, ApplicationCommandContext context, int count) => string.Join(' ',
+                                                                                                someService.GetSomeData()
+                                                                                                           .Take(count)));
 
 host.UseGatewayEventHandlers();
 
